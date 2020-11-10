18a5cda5ad3e84e1a6e19121cd32195e5e1e2539
adamc-e <adameyal@gmail.com>
2020-11-10 16:18:25 +0200

    finished milestone 3, will come back to improve, just crossing the line for the submission deadline

--- .github/index.js
+++ .github/index.js
@@ -11,14 +11,32 @@ const findCompanyData = () => {
   fetch(urlExchangeSearch)
   .then(response => response.json())
   .then(data => { 
-    getCornerDiv.classList.add("spinner-grow","spinner-border-sm");  // convert to 
-    for (let i = 0; i < data.length; i++) {
-      let tempObj = {};
-      tempObj.name = data[i].name;
-      tempObj.symbol = data[i].symbol;
-      sortedData.push(tempObj);
-      addLi(tempObj.name,tempObj.symbol);
-      console.log(data);
+      getCornerDiv.classList.add("spinner-grow","spinner-border-sm");  // convert to 
+      for (let i = 0; i < data.length; i++) {
+        let tempObj = {};
+        tempObj.symbol = data[i].symbol;
+        sortedData.push(tempObj);
+        let symbol = tempObj.symbol;
+        const urlProfile = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`;
+        fetch(urlProfile)
+        .then(response => response.json())
+        .then(data => { 
+          
+
+          addLi(data.profile.companyName,symbol,data.profile.image,data.profile.price,data.profile.changesPercentage);
+
+
+
+
+
+        });
+
+
+
+
+
+
+
     }
     setTimeout(function() {
       getCornerDiv.classList.remove("spinner-grow","spinner-border-sm");
@@ -27,34 +45,56 @@ const findCompanyData = () => {
     .catch(err => console.log('Request Failed', err));
 }
 
-const addLi = (company,ticker) => { 
+
+// then(data => { 
+//   getCornerDiv.classList.add("spinner-grow","spinner-border-sm");  // convert to 
+//   for (let i = 0; i < data.length; i++) {
+//     let tempObj = {};
+//     tempObj.name = data[i].name;
+//     tempObj.symbol = data[i].symbol;
+//     sortedData.push(tempObj);
+//     addLi(tempObj.name,tempObj.symbol);
+//     console.log(data);
+//   }
+//   setTimeout(function() {
+//     getCornerDiv.classList.remove("spinner-grow","spinner-border-sm");
+//   }, 800); //Time in milliseconds
+// }) 
+//   .catch(err => console.log('Request Failed', err));
+// }
+// // 
+
+const addLi = (companyName, symbol, image, price, priceChanges) => { 
   const getSearchUL = document.getElementById("search-list"); //defined here as well to keep my code complete for further use
   getSearchUL.classList.add("list-group"); // adding the bootstrap class to UL
+  var img = document.createElement('img');
+  img.src = `${image}`;
   let newListItem = document.createElement('LI');
+  newListItem.appendChild(img);
   var a = document.createElement('a'); //create new anchor element
-  let liTextItem = document.createTextNode(`${company},${ticker}`);
+  let liTextItem = document.createTextNode(` ${companyName},${symbol}`);
   a.appendChild(liTextItem);
   a.title = "my title text";
-  a.href = `./company.html?symbol=${ticker}`;
+  a.href = `./company.html?symbol=${symbol}`;
   newListItem.appendChild(a);
   getSearchUL.appendChild(newListItem);
+  let liTextItem2 = document.createTextNode(`     ${price},${priceChanges}`);
+  newListItem.appendChild(liTextItem2);
   newListItem.classList.add("list-group-item"); //adding the li bootstrap class style
 }
-
-
-
-async function CompanyData() {
-  const urlProfile = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`;
-  const response = await fetch(urlProfile);
-  const data = await response.json();
-  return data;
-}
-
-fetchCompanyData().then(data => {
-  addLiCompany(data.symbol,data.profile.image,website,data.profile.price,data.profile.changesPercentage);
-});
-
-
+// const addLi = (companyName, symbol, image, website, price, priceChanges) => { 
+//   const getSearchUL = document.getElementById("search-list"); //defined here as well to keep my code complete for further use
+//   getSearchUL.classList.add("list-group"); // adding the bootstrap class to UL
+//   let newListItem = document.createElement('LI');
+//   var a = document.createElement('a'); //create new anchor element
+//   let liTextItem = document.createTextNode(`${companyName},${symbol}`);
+//   a.appendChild(liTextItem);
+//   a.title = "my title text";
+//   a.href = `./company.html?symbol=${symbol}`;
+//   newListItem.appendChild(a);
+//   getSearchUL.appendChild(newListItem);
+//   newListItem.classList.add("list-group-item"); //adding the li bootstrap class style
+// }
 
 
 
